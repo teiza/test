3e5e8b4b34b4717d3fad6acc6981885bb5f01810
Eduard Popkov <eduard.popkov@office.ctco.lv>
2021-07-16 11:24:56 +0300

    m2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,10 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+
+            //helllo
+
+            //new changes
         }
     }
 }
